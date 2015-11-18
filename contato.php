6c4160885210b3d16240b838ed016a991d8f3982
RafaelPohlenz <rafaelpohlenz@gmail.com>
2015-11-17 23:10:19 -0200

    Atualizado
    Correções

--- contato.php
+++ contato.php
@@ -18,7 +18,6 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 
 
-	
 </head>
 <body>
 
@@ -34,19 +33,18 @@
 					<div class="panel-body">
 						<form role="form">
 							<div class="form-group">
-								<label for="Name">Nome:</label>
-								<input type="text" class="form-control" id="nome">
+								<label for="Name">Nome ou Empresa:</label>
+								<input placeholder="Nome ou Empresa:" type="text" class="form-control" id="nome" required>
 							</div>
 							<div class="form-group">
 								<label for="email">Email address:</label>
-								<input type="email" class="form-control" id="email">
+								<input placeholder="exemplo@exemplo.com" type="email" class="form-control" id="email" required>
 							</div>
-							<div class="form-group">
-								<label for="Mensagem">Mensagem:</label>
-								<textarea type="text" class="form-control" id="msg"></textarea>
+							<div class="form-group">								<label  for="Mensagem">Mensagem:</label>
+								<textarea placeholder="Mensagem" type="text" class="form-control" id="mensagem" required></textarea>
 							</div>
 							
-							<button type="submit" class="btn btn-default">Enviar</button>
+							<button type="submit" class="btn btn-default" onclick="validaForm()">Enviar</button>
 						</form>
 					</div>
 				</div>
@@ -54,9 +52,36 @@
 
 		</div>
 
-		<?php include 'rodape.php' ?>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="panel panel-default">
+					<!-- 	Contato-->
+					<div class="panel-heading">Outros meios de contato com a empresa:</div>
+					<div class="panel-body">
+					Telefone: (49)9903-1638<br><br>
+					Email: Iccomercial@yahoo.com.br
 
-		
-	</div>
-</body>
-</html>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="panel panel-default">
+					<!-- 	Contato-->
+					<div class="panel-heading">Endereço</div>
+					<div class="panel-body">
+					Rua Brásilia,1111 -Sala 81 - B. São Cristóvão - Caçador - SC- 89.500-000
+
+					</div>
+				</div>
+			</div>
+		</div>
+					<?php include 'rodape.php' ?>
+
+				</div>
+			</body>
+			</html>
